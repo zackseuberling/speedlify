9bcf6ee908eaee24fbf18325714f47b5564b9c91
Zack Seuberling <seuberling@gmail.com>
2020-07-12 18:14:49 -0700

    remove zachleatherman's sites

---
+++ _data/sites/personal.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "personal", // optional, falls back to object key
+  description: "personal web site",
+  options: {
+    frequency: 60 * 23, // 23 hours
+    freshChrome: "site",
+  },
+  urls: ["https://www.zackseuberling.com/"],
+};

f3e6cdf08e0573547cdde81d6bf50ce2bba7c2ce
Zack Seuberling <seuberling@gmail.com>
2021-01-29 14:39:01 -0800

    chore: add bidoun

--- _data/sites/clients.js
+++ _data/sites/clients.js
@@ -7,5 +7,9 @@ module.exports = {
     // chrome with each run instead of each site in every run (it’s faster)
     freshChrome: "run",
   },
-  urls: ["https://www.lelli.store/", "https://chadkloepfer.com"],
+  urls: [
+    "https://www.lelli.store/",
+    "https://chadkloepfer.com",
+    "https://www.bidoun.org",
+  ],
 };
